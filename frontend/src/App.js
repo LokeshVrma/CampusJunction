a9a75299c52bba467924c53ffe9ac504fd293a67
Luckymishraa <86152604+Luckymishraa@users.noreply.github.com>
2024-09-09 11:23:27 +0530

    add routing to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ForgotPage from './pages/ForgotPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import LostAndFoundPage from './pages/LostAndFoundPage';
+import ProductListingPage from './pages/ProductListingPage';
+import TutorHiringPage from './pages/TutorHiringPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -12,6 +18,21 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
+          <Route path='/forgot' element={<ForgotPage />} />
+          
+          {/* Buy and Sell Products */}
+          <Route path="/products" element={<ProductListingPage />} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
+
+          {/* Hire Tutors */}
+          <Route path="/hire-tutors" element={<TutorHiringPage />} />
+
+          {/* Lost and Found Services */}
+          <Route path="/lost-and-found" element={<LostAndFoundPage />} />
+
+          {/* 404 Page */}
+          <Route path="*" element={<NotFoundPage />} />
+
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/pages/ProductListingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function ProductListingPage(){
+    return (
+        <div>
+            <h1> ProductListingPage</h1>
+        </div>
+    )
+}
+
+export default ProductListingPage;
