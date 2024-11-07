5e72d5c499be0758b39137309bbeadee3888282f
Lokesh Verma <lokeshv613@gmail.com>
2024-11-07 11:26:11 +0530

    update product schema

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Category = require('../models/Categories');
+const Category = require('../models/categories');
 
 router.get('/', async (req, res) => {
   try {
